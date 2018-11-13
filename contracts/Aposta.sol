5379a9f91e850a5f007d72f9dc690f3578003e00
Vitor Bretas Prata <vitor.prata@allmatech.com.br>
2018-11-13 19:02:01 -0200

    Aprimoramento da função reset

--- contracts/Aposta.sol
+++ contracts/Aposta.sol
@@ -9,19 +9,20 @@ contract BetContract{
         addTeam('Team 1');
         addTeam('Team 2');
     } 
+    
+    function () public payable { }
 
     struct Team{
         uint8 ID;
         string Name;
         uint amountWagered;    
-        bool chosen;        
+        bool chosen;   
+        address punter;
     }  
 
     event betEvent (
         uint indexed _TeamID        
     ); 
-
-    function () external payable {  }
     
     function getBalance() public view returns(uint){
         return address(this).balance; 
@@ -35,12 +36,11 @@ contract BetContract{
     mapping(address => bool) public bettors;
 
     uint8 public teamNumber;
-    uint8 public numberOfBettors;   
-    uint private Amount; 
+    uint8 public numberOfBettors; 
 
     function addTeam(string _name) private {
         teamNumber++;
-        Teams[teamNumber] = Team(teamNumber, _name, 0, false);
+        Teams[teamNumber] = Team(teamNumber, _name, 0, false, this);
     }    
     
     function Bet(uint ID) public payable {
@@ -52,31 +52,35 @@ contract BetContract{
         require(!Teams[ID].chosen);
         
         Teams[ID].chosen = true;
-        Amount = msg.value;
+        Teams[ID].punter = msg.sender;
         bettors[msg.sender] = true;
-        Teams[ID].amountWagered += Amount;        
+        Teams[ID].amountWagered += msg.value;        
         numberOfBettors++;
         
-        address(_instance).transfer(Amount);
-        _instance.storeBalance(msg.sender, ID, Amount);
+        address(_instance).transfer(Teams[ID].amountWagered);
+        _instance.storeBalance(msg.sender, ID);
         
         emit betEvent(ID);
     }  
     
     function getWinner() public returns (uint){
-        return _instance.announceWinner();
-        selfdestruct(_instance);
+        uint Winner = _instance.announceWinner();
+        resetContract();
+        return Winner;
     }
     
     function getTeamsInstance(uint8 _id) public view returns (address) {
         return _instance.getBettors(_id);
     }
 
-    function resetContract() private returns (uint8) {
+    function resetContract() internal returns (uint8) {
         numberOfBettors = 0;
         teamNumber = 0;
+        bettors[Teams[1].punter] = false;
+        bettors[Teams[2].punter] = false;
         addTeam('Team 1');
         addTeam('Team 2');
+        delete _instance;
     }
 }
 
@@ -91,15 +95,9 @@ contract Oracle{
         addTeam(1);
         addTeam(2);
     } 
-
-    function resetContract() internal returns(uint) {
-        Winner = 0;
-        addTeam(1);
-        addTeam(2);
-    } 
     
     mapping(uint => Team) public Teams;
-
+    
     function () public payable { }
 
     function addTeam(uint ID) private {        
@@ -112,13 +110,12 @@ contract Oracle{
     
     uint public amountStored;
     uint public Winner;
-
     
-    function storeBalance(address _punter, uint _idTime, uint _balance) payable public {
+    function storeBalance(address _punter, uint _idTime) payable public {
         Teams[_idTime].ID = _idTime;
         Teams[_idTime].punter = _punter;
         
-        amountStored += _balance;
+        amountStored += msg.value;
     }
     
     function getBettors(uint8 _id) public view returns (address){
